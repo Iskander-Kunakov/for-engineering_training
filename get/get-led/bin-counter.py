01bd5da2e1194332a83888da0262230f1118f1a1
Искандер Кунаков <heterosexual235@gmail.com>
2026-04-27 11:04:07 +0300

    bin-counter

--- get/get-led/bin-counter.py
+++ get/get-led/bin-counter.py
@@ -1,24 +1,24 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as rpg
 import time
-GPIO.setmode(GPIO.BCM)
+rpg.setmode(rpg.BCM)
 leds=[16,12,25,17,27,23,22,24]
-GPIO.setup(leds, GPIO.OUT)
-GPIO.output(leds, 0)
+rpg.setup(leds, rpg.OUT)
+rpg.setup([9, 10], rpg.OUT)
+rpg.output(leds, 0)
 num=0
-up=GPIO.input(9)
-down=GPIO.input(10)
-def dec2bin(value):
-    return [int(element) for element in bin(value)[2:].zfill(8)]
-sleep_time=0.2
+
 while True:
-    up=GPIO.input(9)
-    down=GPIO.input(10)
-    if GPIO.input(up):
-        num+=1
-        print(num, dec2bin(num))
-        time.sleep(sleep_time)
-    if GPIO.input(down):
-        num-=1
-        print(num, dec2bin(num))
-        time.sleep(sleep_time)
-    GPIO.output(leds, dec2bin(num))
+    if rpg.input(9)*rpg.input(10):
+        num = 256
+    if rpg.input(9):
+        if num < 255:
+            num+=1
+    if rpg.input(10):
+        if num > 0:
+            num-=1
+    time.sleep(0.2)
+    b=list(bin(num))[2:]
+    s=list(map(int, b))
+    c=[0]*(8-len(b))+s
+    print(num, c)
+    rpg.output(leds, c)
