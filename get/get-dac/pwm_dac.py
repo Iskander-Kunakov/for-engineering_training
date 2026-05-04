4bc2d0f77b72e788f59853bf30ddf7fbf1ca78f5
Искандер Кунаков <heterosexual235@gmail.com>
2026-05-04 10:12:55 +0300

    for CAP-2

--- get/get-dac/pwm_dac.py
+++ get/get-dac/pwm_dac.py
@@ -1,72 +1,52 @@
-import RPi.GPIO as GPIO
-
-gpio_pin  = 12
-
+import RPi.GPIO as rpg
 class PWM_DAC:
     def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
-        self.gpio_pin = gpio_pin
-        self.pwm_frequency = pwm_frequency
-        self.dynamic_range = dynamic_range
+        self.pin = gpio_pin
+        self.freq = pwm_frequency
+        self.dyrange = dynamic_range
         self.verbose = verbose
 
-<<<<<<< HEAD
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_pin, GPIO.OUT)
-
-        self.pwm = GPIO.PWM(self.gpio_pin, self.pwm_frequency)
-        self.pwm.start(0)
-
-    def deinit(self):
-        self.pwm.stop()
-        GPIO.cleanup()
-    
-    def set_voltage(self, voltage):
-        if not (0.0 <= voltage <= self.dynamic_range):
-            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
-            print("Устанавливаем 0.0 В")
-            voltage = 0.0
-
-        duty_cycle = (voltage/ self.dynamic_range) * 100
-        self.pwm.ChangeDutyCycle(duty_cycle)
-
-if __name__ == "__main__":
-    try:
-        dac = PWM_DAC(12, 1000, 3.290, True)
-
-        while True:
-            try:
-                voltage = float(input("Введите напряжение в вольтах: "))
-                dac.set_voltage(voltage)
-            
-            except ValueError:
-                print("Вы ввели не число \n")
-    
-=======
         rpg.setmode(rpg.BCM)
         rpg.setup(self.pin, rpg.OUT, initial = 0)
     def deinit(self):
         rpg.output(self.pin, 0)
         rpg.cleanup()
-    def set_voltage(self, voltage):
-        if not (0.0<=voltage <=self.dynamic_range):
-            print('Напряжение выходит за динамический диапазон ЦАП')
-            print("Устанавливаем 0.0 В")
-            return 0
-        GPIO.output(self.gpio_bits, self.set_number(int(voltage / self.dynamic_range * 255)))
-        return int(voltage / self.dynamic_range * 255)
-
-if __name__=="__main__":
-    try:
-        dac = PWM_DAC(12, 500, 3.290, True)
-
-        while True:
+    def setvol(self, v):
+        try:
+            p=rpg.PWM(self.pin, self.freq)
+            p.start(v/self.dyrange*100)
+            input('press enter to stop')
+            p.stop()
+        finally:
+            self.deinit()
+        if __name__ == '__main__':
             try:
-                voltage=float(input("Введите напряжение в Вольтах: "))
-                dac.set_voltage(voltage)
-            
-            except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз")
+                dac=PWM_DAC(12, 500, 3.1, True)
+                dac.setvol(1.2)
+            finally:
+                self.deinit()
 
->>>>>>> 0108e22c1a2c05496db5c449aeb8f58e59361471
-    finally:
-        dac.deinit()
+
+class PWM_DAC_sin:
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
+        self.pin = gpio_pin
+        self.freq = pwm_frequency
+        self.dyrange = dynamic_range
+        self.verbose = verbose
+        rpg.setmode(rpg.BCM)
+        rpg.setup(self.pin, rpg.OUT, initial = 0)
+    def deinit(self):
+        rpg.output(self.pin, 0)
+        rpg.cleanup()
+    def setvol(self, v):
+        try:
+            p=rpg.PWM(self.pin, self.freq)
+            p.start(v)
+        finally:
+            self.deinit()
+        if __name__ == '__main__':
+            try:
+                dac=PWM_DAC(12, 500, 3.1, True)
+                dac.setvol(1.2)
+            finally:
+                self.deinit()
